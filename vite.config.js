1a99600eb69e16f71f0b0cf530be990b3d188351
gudlayv <rotachev.g@gmail.com>
2022-05-14 01:26:03 +0300

    Added deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
-
+import packageConfig from "./package.json";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: packageConfig.homepage,
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
